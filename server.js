61f687e56eb59cc4c111d662184ae90ef5a0eee1
Trevor Bruce <tbnyk03@gmail.com>
2021-05-02 22:58:01 -0600

    Routes

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('./Routes/apiRoute')(app);
+require('./Routes/htmlRoute')(app);
+
 app.listen(PORT, () => console.log(`App listening on Port ${PORT}`))
 
