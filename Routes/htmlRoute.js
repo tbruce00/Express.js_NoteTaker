328b5f786208672889ada0964abe0c0316eacbb1
Trevor Bruce <tbnyk03@gmail.com>
2021-05-03 22:06:36 -0600

    Fixed index.js path

--- Routes/htmlRoute.js
+++ Routes/htmlRoute.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 module.exports = (app) => {
     app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, 'Express.js_NoteTaker/public/notes.html'));
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'Express.js_NoteTaker/public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'Express.js_NoteTaker/public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     })
 };
 
