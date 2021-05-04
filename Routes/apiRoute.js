4c891e06852986d80880a881b34f5c8000f92dd5
Trevor Bruce <tbnyk03@gmail.com>
2021-05-03 22:36:01 -0600

    heroku push

--- Routes/apiRoute.js
+++ Routes/apiRoute.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const uuid = require('uuid');
 const path = require('path');
-const { doesNotThrow } = require('assert');
+// const { doesNotThrow } = require('assert');
 
 
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        fs.readFileSync('Express.js_NoteTaker/db/db.json', (err, data) => {
+        fs.readFileSync('../db/db.json', (err, data) => {
             if(err) throw err;
             return res.json(JSON.parse(data))
         })
@@ -15,13 +15,13 @@ module.exports = (app) => {
 });
     app.post('/api/notes', (req, res) => {
         const postNote = req.body;
-        fs.readFile('Express.js_NoteTaker/db/db.json', (err, data) => {
+        fs.readFile('../db/db.json', (err, data) => {
             if(err) throw err;
             const note = JSON.parse(data)
             postNote.id = uuid.v4()
             note.push(postNote);
             stringNote = JSON.stringify(note);
-            fs.writeFile('Express.js_NoteTaker/db/db.json', stringNote, (err, data) => {
+            fs.writeFile('../db/db.json', stringNote, (err, data) => {
                 return res.json(postNote)
             })
 
