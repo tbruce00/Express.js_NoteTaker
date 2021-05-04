133aef9c160f9f61d86ad8aff4b3634e06868b40
Trevor Bruce <tbnyk03@gmail.com>
2021-05-03 21:46:56 -0600

    Fixed paths

--- Routes/htmlRoute.js
+++ Routes/htmlRoute.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 module.exports = (app) => {
     app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+        res.sendFile(path.join(__dirname, 'Express.js_NoteTaker/public/notes.html'));
     });
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, './Develop/public.index.html'));
+        res.sendFile(path.join(__dirname, 'Express.js_NoteTaker/public/index.html'));
     });
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+        res.sendFile(path.join(__dirname, 'Express.js_NoteTaker/public/index.html'));
     })
 };
 
